079e31b4ad11113ddc4bfc6ef632cb329ea004ca
Marvin Reich <mreich@gfz-potsdam.de>
2017-12-14 01:21:13 +0100

    modified pick_ts.R in order for hourly input files

--- R/pick_ts.R
+++ R/pick_ts.R
@@ -24,19 +24,18 @@ pick_ts = function(
     # input_dir = dir_input
     # site_name = site
     # variation = site_component_combinations[1,]
-    # facTopo = 0.418
     #######
     ## load site parameters
     facTopo = getStationParam(site_name, "facTopo")
     ## load time series
     ####################
     # atmosphere
-    atmosphere_daily = read_data(
+    atmosphere_hourly = read_data(
         data_in = paste0(site_name, "_atmosphere_hourly.tsf"),
         data_dir = paste0(input_dir, "Atmosphere/"),
         dat_tsf = (variation$atmo + 6))
     # change column name
-    colnames(atmosphere_daily)[2] = "atmo"
+    colnames(atmosphere_hourly)[2] = "atmo"
     ####################
     # # discharge
     # load(file = paste0(input_dir, "Discharge/", site_name, "_discharge_daily.rData"))
@@ -47,68 +46,72 @@ pick_ts = function(
     #     dplyr::select(datetime, discharge)
     ####################
     # global hydrology
-    globHyd_daily = read_data(
+    globHyd_hourly = read_data(
         data_in = paste0(site_name, "_globalHydrology_hourly.tsf"),
         data_dir = paste0(input_dir, "GlobalHydrology/"),
         dat_tsf = (variation$globHyd + 6))
     # change column name
-    colnames(globHyd_daily)[2] = "globHyd"
+    colnames(globHyd_hourly)[2] = "globHyd"
     ####################
     # non tidal ocean loading
-    ntol_daily = read_data(
+    ntol_hourly = read_data(
         data_in = paste0(site_name, "_nonTidalOceanLoading_hourly.tsf"),
         data_dir = paste0(input_dir, "NonTidalOceanLoading/"),
         dat_tsf = (variation$ntol + 6))
     # change column name
-    colnames(ntol_daily)[2] = "ntol"
+    colnames(ntol_hourly)[2] = "ntol"
     ####################
-    # precipitation: precip_daily
-    load(file = paste0(input_dir, "Precipitation/", site_name, "_precipitation_daily.rData"))
+    # precipitation: precip_hourly
+    load(file = paste0(input_dir, "Precipitation/", site_name, "_precipitation_hourly.rData"))
     # convert from mm to gravity
     # using topography factor
-    precip_daily = precip_daily %>%
+    precip_hourly = precip_hourly %>%
         dplyr::mutate(precip = value * facTopo) %>%
         dplyr::select(datetime, precip)
     ####################
     # tides
-    tides_daily = read_data(
+    tides_hourly = read_data(
         data_in = paste0(site_name, "_tides_hourly.tsf"),
         data_dir = paste0(input_dir, "Tides/"),
         dat_tsf = (variation$tides + 6))
     # change column name
-    colnames(tides_daily)[2] = "tides"
+    colnames(tides_hourly)[2] = "tides"
     ####################
-    # 
+     
     ####################
     ## combine time series
     # include evapotranspiration ??
     if(includeET){ # yes
-        load(file = paste0(input_dir, "Evapotranspiration/", site_name, "_evapotranspiration_daily.rData"))
+        # daily
+        # load(file = paste0(input_dir, "Evapotranspiration/", site_name, "_evapotranspiration_daily.rData"))
+        # hourly
+        load(file = paste0(input_dir, "Evapotranspiration/", site_name, "_evapotranspiration_hourly.rData"))
         # convert from mm to gravity
         # using topography factor
-        ET_daily = ET_daily %>%
+        ET_hourly = ET_hourly %>%
             dplyr::mutate(ET = value * facTopo) %>%
             dplyr::select(datetime, ET)
         ## combine time series
-        gravity_ts = ET_daily %>%
-            dplyr::inner_join(atmosphere_daily) %>%
+        gravity_ts = ET_hourly %>%
+            dplyr::inner_join(atmosphere_hourly) %>%
             # dplyr::inner_join(discharge_daily) %>%
-            dplyr::inner_join(globHyd_daily) %>%
-            dplyr::inner_join(ntol_daily) %>%
-            dplyr::inner_join(precip_daily) %>%
-            dplyr::inner_join(tides_daily) %>%
+            dplyr::inner_join(globHyd_hourly) %>%
+            dplyr::inner_join(ntol_hourly) %>%
+            dplyr::inner_join(precip_hourly) %>%
+            dplyr::inner_join(tides_hourly) %>%
             # make sure there is no NA value !!
             # convert to 0
-            dplyr::mutate(value = ET + atmo + discharge + globHyd + ntol + precip + tides) %>%
+            # dplyr::mutate(value = ET + atmo + discharge + globHyd + ntol + precip + tides) %>%
+            dplyr::mutate(value = ET + atmo + globHyd + ntol + precip + tides) %>%
             dplyr::select(datetime, value)
     }else{ # no
         ## combine time series
-        gravity_ts = atmosphere_daily %>%
+        gravity_ts = atmosphere_hourly %>%
             # dplyr::inner_join(discharge_daily) %>%
-            dplyr::inner_join(globHyd_daily) %>%
-            dplyr::inner_join(ntol_daily) %>%
-            dplyr::inner_join(precip_daily) %>%
-            dplyr::inner_join(tides_daily) %>%
+            dplyr::inner_join(globHyd_hourly) %>%
+            dplyr::inner_join(ntol_hourly) %>%
+            dplyr::inner_join(precip_hourly) %>%
+            dplyr::inner_join(tides_hourly) %>%
             # make sure there is no NA value !!
             # convert to 0
             # dplyr::mutate(value = atmo + discharge + globHyd + ntol + precip + tides) %>%
